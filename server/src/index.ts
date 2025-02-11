26bcd3c7a49ca1ac474919eef19e3cd7b545ca97
ThomasLaforge <thomas.laforge.38@gmail.com>
2025-02-11 15:54:13 +0100

    initial commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,19 @@
+import cors from 'cors'
 import "dotenv/config"
 import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
 
 const app = express()
+
 app.use(cors())
-app.use(bodyParser.json())
-const port = process.env.PORT || 3000
+
+const port = process.env.PORT || 1992
 
 app.get('/hello/:name', (req, res) => {
   res.send('Hello petit ' + req.params.name)
 })
 
 app.get('/server/time', (req, res) => {
-  res.send({ time: Date.now() })
+  res.json({ time: Date.now() })
 })
 
 app.listen(port, () => {
